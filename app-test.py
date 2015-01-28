d291207bd081f59ec3a084ced4de12323430ff84
Sabdhagiri Govindaraju <sabdhagiri@onecloudinc.com>
2015-01-28 04:29:43 -0500

    modified test case for app.py index
    Change-Id: Ieee85d8e7fead75fa202cd767a06947c13692fb2

--- app-test.py
+++ app-test.py
@@ -2,7 +2,7 @@ from app import app
 import unittest
 
 class BasicTestCase(unittest.TestCase):
-    def test_index(self):
+    def test_idx(self):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)

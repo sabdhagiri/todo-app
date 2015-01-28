a663cd44bcdb7a0034ec7fbe345c490dc233be70
Sabdhagiri Govindaraju <sabdhagiri@onecloudinc.com>
2015-01-28 06:10:21 -0500

    Modified app.py to include configuration directives
    Change-Id: I1810e2c5fba0d7a6a54575859cd612ce6ef749db

--- app-test.py
+++ app-test.py
@@ -2,11 +2,10 @@ from app import app
 import unittest
 
 class BasicTestCase(unittest.TestCase):
-    def test_idx(self):
+    def test_index(self):
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, "Hello, world!")
+        self.assertEqual(response.status_code, 404)        
 
 if __name__ == '__main__':
     unittest.main()
--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
-import os
+from flask import Flask, request, session, g, redirect, url_for, render_template, abort, flash, jsonify
+import sqlite3
+
+# Configuration
+
+DATABASE='flaskr.db'
+DEBUG = True
+SECRET_KEY = 'secret'
+USERNAME = 'admin'
+PASSWORD = 'password'
+
 
 app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return 'Hello, world!'
+app.config.from_object(__name__)
 
 if __name__ == '__main__':
     app.run()
